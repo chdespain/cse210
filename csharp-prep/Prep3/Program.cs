3319b66a984f46436fc6211b266cccb5e30a3e47
chdespain <u520787507@byui.edu>
2023-01-17 09:55:25 -0500

    Finished Prep 3 and 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is your magic number? ");
+        // string magicNumberS = Console.ReadLine();
+        // int magicNumber = int.Parse(magicNumberS);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+    
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the magic number! ");
+            string guessS = Console.ReadLine();
+            guess = int.Parse(guessS);
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        // Console.Write("Input a number to add: ");
+
+        int numberToAdd;
+        do
+        {
+            Console.Write("Input a number to add: ");
+            numberToAdd = int.Parse(Console.ReadLine());
+            numbers.Add(numberToAdd);
+        } while (numberToAdd != 0);
+
+        int sum = 0;
+        foreach (int entry in numbers)
+        {
+            sum += entry;
+        }
+        String output = "The sum is " + sum + ".";
+        Console.WriteLine(output);
+
+        int numberOfEntries = numbers.Count;
+        float avg = sum / numberOfEntries;
+        output = "The average is " + avg + ".";
+        Console.WriteLine(output);
+
+        int largest = numbers.Max();
+        output = "The largest number is " + largest + ".";
+        Console.WriteLine(output);
+
     }
 }
